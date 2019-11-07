3e2e4d13954cddf3776799182adbd9d198135633
pasqualeparillo <patparillo@gmail.com>
2019-11-07 12:44:55 -0500

    proptypes test

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,26 +1,34 @@
-import React from 'react';
-import './layout.css';
-import { ContextProvider } from '../store/state';
-import NavIndex from '../navigation';
-import { AnimatePresence, motion } from 'framer-motion';
+import React from "react";
+import "./layout.css";
+import { ContextProvider } from "../store/state";
+import NavIndex from "../navigation";
+import { AnimatePresence, motion } from "framer-motion";
+import PropTypes from "prop-types";
 
 const Layout = ({ children, location }) => (
-	<ContextProvider>
-		<div className="flex flex-col self-center items-center min-h-screen full justify-between relative overflow-hidden bg-black">
-			<NavIndex />
-			<AnimatePresence exitBeforeEnter initial={false}>
-				<motion.div
-					className="lg:w-11/12 w-full flex mx-auto flex-1 overflow-hidden bg-white"
-					initial="exit"
-					animate="enter"
-					exit="exit"
-					key={location.pathname}
-				>
-					{children}
-				</motion.div>
-			</AnimatePresence>
-		</div>
-	</ContextProvider>
+  <ContextProvider>
+    <div className="flex flex-col self-center items-center min-h-screen full justify-between relative overflow-hidden bg-black">
+      <NavIndex />
+      <AnimatePresence exitBeforeEnter initial={false}>
+        <motion.div
+          className="lg:w-11/12 w-full flex mx-auto flex-1 overflow-hidden bg-white"
+          initial="exit"
+          animate="enter"
+          exit="exit"
+          key={location.pathname}
+        >
+          {children}
+        </motion.div>
+      </AnimatePresence>
+    </div>
+  </ContextProvider>
 );
+
+Layout.propTypes = {
+  children: PropTypes.node.isRequired
+};
+Layout.defaultProps = {
+  location: {}
+};
 
 export default Layout;
